5af3a60fb72cd41ee02b64a928cc5bc7f6771bef
TheEpicBlock <61842090+TheEpicBlock@users.noreply.github.com>
2020-10-31 17:31:37 +0100

    Init repo

---
+++ src/main/java/nl/theepicblock/mcjoininvincibility/McJoinInvincibility.java
@@ -0,0 +1,10 @@
+package nl.theepicblock.mcjoininvincibility;
+
+import net.fabricmc.api.ModInitializer;
+
+public class McJoinInvincibility implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		
+	}
+}
